85a4e3cfde4796b86d3215df644434bd49deaa29
bairog <44995181+bairog@users.noreply.github.com>
2020-02-19 10:33:12 +0300

    first commit

---
+++ Autofac5ContainerTest/Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using Interfaces;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using System;
+using Autofac.Configuration;
+
+namespace UnityAutofac5ContainerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder();
+            config.AddJsonFile("config.json");
+
+            var module = new ConfigurationModule(config.Build());
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule(module);
+            //builder.RegisterType<ImplementationN>().As<InterfaceN>();
+
+            var container = builder.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var impl = scope.Resolve<InterfaceN>();
+                Console.WriteLine(impl.GetString());
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
